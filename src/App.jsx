7f2d4a89b072f078ad5c5ce92d40e3323ff037ff
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2025-11-13 15:08:56 +0200

    add todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import { useState } from "react";
 import "./App.css"
+import TodoList from "./components/TodoList.jsx";
 function Counter() {
-    // יש לכתוב תוכנית שמציגה למשתמש שדה קלט תגית של שם ,ו3 כפתורים
-    // כפתור להצגה בקונסול, כפתור להצגה במסך , וכפתור להצגה ב -Alert
-    // המשתמש יכניס את השם שלו ילחץ על הכפתור נדפיס את השם במקום המתאים לפי בחירת המשתמש
 
-   const [name, setName] = useState("");
-
-    const showNameC = () => {
-       console.log(name);
-    };
-
-    const showNameA = () => {
-    alert(name)
-    };
 
     return (
         <div>
-            <h1>{name}</h1>
-            <input value={name} onChange={e => setName(e.target.value)} />
-            <br/>
-            <button onClick={showNameA}>name with alert</button>
-            <button onClick={showNameC}>name with console</button>
-            {/*<button onClick={showName}>name with screen</button>*/}
+         <TodoList/>
         </div>
     );
 }
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,66 @@
+ import React, {useState} from 'react';
+ import "./TodoList.css"
+
+ function TodoList(){
+    const [task, setTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+    const [index, setIndex] = useState(null);
+
+    const addTask =()=>{
+        if (index===null){
+            setTasks([...tasks,task])
+        }else {
+            const updateTasks = tasks;
+            updateTasks[index] = task
+            setTasks(updateTasks);
+            setIndex(null)
+        }
+
+        setTask("")
+    }
+
+    const editTask =(index)=>{
+        setIndex(index)
+        setTask(tasks[index])
+    }
+
+    const deleteTask =()=>{
+       setTasks(tasks.filter(((task,i) => i !== index)))
+        setIndex(null)
+    }
+
+     return(
+         <div>
+            <h1>Todo List</h1>
+
+             <input
+                 type={"text"}
+                 placeholder={"Add task ..."}
+                 value={task}
+                 onChange={(e) => setTask(e.target.value)}
+             />
+             <button disabled={task.trim()===""}
+                     onClick={addTask}
+             >{index===null?"Add":"Edit"}</button>
+
+             {
+                 tasks.length>0 && (
+                     <ul>
+                         {
+                             tasks.map((task, index) =>
+                                 <li key={index}>
+                                     {task}
+                                     <button onClick={()=>editTask(index)}>Edit</button>
+                                     <button onClick={deleteTask}>Delete</button>
+                                 </li>
+                             )
+                         }
+                     </ul>
+                 )
+             }
+         </div>
+     )
+
+ }
+ export default TodoList;
+
